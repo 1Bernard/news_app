e5ca1469bc0db22038f9fce42c0b8749eb64f90a
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-12-06 12:43:57 +0000

    add retrofit and paging

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val USER_SETTINGS = "userSettings"
 
     const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "9a02054d41e04a528c961b65fd29c415 9a02054d41e04a528c961b65fd29c415"
 }
